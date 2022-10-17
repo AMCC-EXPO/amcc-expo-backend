83dab230bfbaa576bd875101ff81f462795c81b1
Muhammad Mufti Ashshiddiqi <muftishid@gmail.com>
2022-10-17 15:19:51 +0700

    feat: update carousel

--- resources/views/layouts/partials/carousel.blade.php
+++ resources/views/layouts/partials/carousel.blade.php
@@ -2,13 +2,22 @@
     <div class="flex h-screen justify-center items-center">
         <div class="owl-carousel mt-9 relative p-5">
             <div class="hero-img md:h-full">
-                <img src="{{ asset('user/img/carousel1.png') }}" alt="img1" />
+                <img src="{{ asset('img/banner1.webp') }}"/>
             </div>
             <div class="hero-img md:h-full">
-                <img src="{{ asset('user/img/carousel1.png') }}" alt="img2" />
+                <img src="{{ asset('img/banner2.webp') }}"/>
             </div>
             <div class="hero-img md:h-full">
-                <img src="{{ asset('user/img/carousel1.png') }}" alt="img3" />
+                <img src="{{ asset('img/banner3.webp') }}"/>
+            </div>
+            <div class="hero-img md:h-full">
+                <img src="{{ asset('img/banner4.webp') }}"/>
+            </div>
+            <div class="hero-img md:h-full">
+                <img src="{{ asset('img/banner5.webp') }}"/>
+            </div>
+            <div class="hero-img md:h-full">
+                <img src="{{ asset('img/banner6.webp') }}"/>
             </div>
         </div>
     </div>
