8a516f39b6214720ff99efcc7b39a6cabc49bfb0
Muhammad Mufti Ashshiddiqi <muftishid@gmail.com>
2022-10-18 07:30:16 +0700

    refactor: fix typo

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -22,7 +22,7 @@
         <div id="dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
             <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
                 <li>
-                    <a href="{{ \App\Models\Setting::first()->link_group }} }}" target="_blank"
+                    <a href="{{ \App\Models\Setting::first()->link_group }}" target="_blank"
                         class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                         Join grup whatsapp
                     </a>
